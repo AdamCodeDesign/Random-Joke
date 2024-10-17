050c4e6648fb7184a1c989c43e5b2a31369b8686
LeshchuDev <leshchudev@gmail.com>
2024-10-17 20:59:02 +0200

    server loads jokes and random joke

---
+++ server.js
@@ -0,0 +1,36 @@
+const http = require("http");
+const {
+  getJokes,
+  getJoke,
+  getRandom,
+} = require("./controllers/jokeController");
+
+const PORT = 8080;
+const API_CONTENT_TYPE = { "Content-type": "application/json" };
+const server = http.createServer(async function (req, res) {
+  console.log("Request");
+
+  if (req.url === "/api/jokes" && req.method === "GET") {
+    let jokes = await getJokes();
+
+    if (jokes) {
+      res.writeHead(200, API_CONTENT_TYPE);
+    } else {
+      res.writeHead(404, API_CONTENT_TYPE);
+      jokes = { message: "Jokes not found" };
+    }
+
+    res.end(JSON.stringify(jokes));
+  } else if (req.url === "/api/jokes/random" && req.method === "GET") {
+    let joke = await getRandom();
+    if (joke) {
+      res.writeHead(200, API_CONTENT_TYPE);
+    } else {
+      res.writeHead(404, API_CONTENT_TYPE);
+      joke = { message: "Jokes not found" };
+    }
+
+    res.end(JSON.stringify(joke));
+  }
+});
+server.listen(PORT);
